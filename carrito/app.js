ce19f8edd32e1318d46d75905d980ebdd0d55b81
Cristian <cristian7368@gmail.com>
2022-01-09 12:27:52 -0400

    all

--- carrito/app.js
+++ carrito/app.js
@@ -24,14 +24,6 @@ app.use('/users', usersRouter);
 
 app.use(cors());
 
-
-// enable CORS without external module
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
